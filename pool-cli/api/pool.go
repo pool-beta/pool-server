ca908cd55066526dca0d4ad5da349d8fdf34cfc0
Soung Bae Kim <soungbaekim99@gmail.com>
2021-03-07 19:46:24 -0800

    no nested maps for pool-cli requests

--- pool-cli/api/pool.go
+++ pool-cli/api/pool.go
@@ -4,20 +4,24 @@ import (
 	"io/ioutil"
 	"fmt"
 	"bytes"
+	"strconv"
 	"encoding/json"
+
+	"github.com/pool-beta/pool-server/daemon/handlers/models"
 )
 
 func RunPoolGet(ctx Context, username string, password string, pool_id string) {
 	fmt.Printf("Getting Pool List: %v\n", ctx.Url())
 
+	var request models.RequestGetPool
+	pid, _ := strconv.ParseUint(pool_id, 10, 64)
+
+	request.UserAuth.UserName = username
+	request.UserAuth.Password = password
+	request.ID = pid
+
 	route := ctx.Url() + "/pool/poolid"
-	data, _ := json.Marshal(map[string]string{
-		"user_auth": map[string]string{
-			"username": username, 
-			"password": password,
-		},
-		"pool_id": pool_id,
-	})
+	data, _ := json.Marshal(request)
 	
 	body := bytes.NewBuffer(data)
 
